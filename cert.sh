8089047a84071ae07cbd0969e7e6fa2e54224bd6
linzell <linzellart@gmail.com>
2023-09-08 01:17:20 +0200

    🎉

--- cert.sh
+++ cert.sh
@@ -1,4 +1,4 @@
-#!/bin/sh
+#!/bin/bash
 
 # Load environment variables
 [ ! -f .env ] || export $(grep -v '^#' .env | xargs)
@@ -10,14 +10,39 @@ else
     SERVER="$1"
 fi
 
-# Generate the certificate
+# Generate the certificate authority (CA)
 openssl req -x509 -nodes  \
   -newkey rsa:4096  \
   -days 365  \
-  -subj "/CN=$SERVER"  \
-  -out cert.pem  \
-  -keyout key.pem  \
+  -subj "/C=CA/ST=None/L=Earth/O=${DB_NAMESPACE}/OU=${DB_USERNAME}/CN=$SERVER"  \
+  -out ca.pem  \
+  -keyout ca.key  \
   -sha256
-openssl rsa -in key.pem -out nopass.pem
-rm key.pem
-mv nopass.pem key.pem
+
+# Generate the client certificate
+openssl req -nodes   \
+  -newkey rsa:2048   \
+  -keyout key.pem \
+  -out "$SERVER".csr    \
+  -subj "/C=CA/ST=None/L=Earth/O=${DB_NAMESPACE}/OU=${DB_USERNAME}/CN=$SERVER:${PORT}"
+openssl x509 -req \
+  -CA ca.pem  \
+  -CAkey ca.key  \
+  -in "$SERVER".csr \
+  -out cert.pem  \
+  -days 365 \
+  -CAcreateserial \
+  -extfile <(cat <<END
+basicConstraints = CA:FALSE
+nsCertType = server
+nsComment = "OpenSSL Generated Server Certificate"
+subjectKeyIdentifier = hash
+authorityKeyIdentifier = keyid,issuer
+keyUsage = digitalSignature, keyEncipherment
+extendedKeyUsage = serverAuth
+subjectAltName = DNS:$SERVER:${PORT}
+END
+    )
+
+# Clean up
+rm -f "$SERVER".csr
