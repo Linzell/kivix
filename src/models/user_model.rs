ce59648418add3ce0457adccc6b441b3342f59a4
linzell <linzellart@gmail.com>
2023-09-14 21:44:50 +0200

    📝

--- src/models/user_model.rs
+++ src/models/user_model.rs
@@ -1,4 +1,5 @@
 // models/user_model.rs
+use argon2::{Argon2, PasswordHash, PasswordVerifier};
 use serde::{Deserialize, Serialize};
 use surrealdb::sql::Thing;
 
@@ -6,50 +7,50 @@ use super::model::{ConnectionData, DBConnection, CRUD};
 use crate::prelude::Error;
 
 /// User Struct
-/// 
+///
 /// ## Fields
-/// 
+///
 /// * `ID` is the user's unique identifier
-/// * `CID` is the user's unique identifier
+/// * `PeerID` is the user's unique identifier
 /// * `Name` is the user's name
 /// * `Avatar` is the user's avatar
 /// * `Email` is the user's email
-/// * `Password` is the user's password
+/// * `PasswordHash` is the user's password hash
 /// * `CreationDate` is the user's creation date
 /// * `IsVisible` is the user's visibility
 /// * `IsInactive` is the user's inactivitys
 #[derive(Debug, Serialize, Deserialize, Clone)]
 pub struct User {
     pub id: Thing,
-    pub cid: String,
+    pub peer_id: String,
     pub name: String,
     pub avatar: String,
     pub email: String,
-    pub password: String,
+    pub password_hash: String,
     pub creation_date: String,
     pub is_visible: bool,
     pub is_inactive: bool,
 }
 
 /// User Create Struct
-/// 
+///
 /// ## Fields
-/// 
-/// * `CID` is the user's unique identifier
+///
+/// * `PeerID` is the user's unique identifier
 /// * `Name` is the user's name
 /// * `Avatar` is the user's avatar
 /// * `Email` is the user's email
-/// * `Password` is the user's password
+/// * `PasswordHash` is the user's password hash
 /// * `CreationDate` is the user's creation date
 /// * `IsVisible` is the user's visibility
 /// * `IsInactive` is the user's inactivitys
 #[derive(Debug, Serialize, Deserialize, Clone)]
 pub struct UserCreate {
-    pub cid: String,
+    pub peer_id: String,
     pub name: String,
     pub avatar: String,
     pub email: String,
-    pub password: String,
+    pub password_hash: String,
     pub creation_date: String,
     pub is_visible: bool,
     pub is_inactive: bool,
@@ -58,34 +59,109 @@ pub struct UserCreate {
 #[async_trait::async_trait]
 impl CRUD<User, UserCreate> for User {
     /// Initialize the user table
-    /// 
+    ///
     /// ## Arguments
     /// ```
     /// db is the database connection
     /// ```
-    /// 
+    ///
     /// ## Filters
     /// ```
     /// email is the user's email
-    /// cid is the user's unique identifier
+    /// peer_id is the user's unique identifier
     /// ```
-    /// 
+    ///
     /// ## Returns
     /// Initializes the user table with the given databases
     async fn init_table(db: DBConnection) -> Result<(), Error> {
         let sql = "DEFINE TABLE users SCHEMAFULL;\
                 DEFINE FIELD email ON users TYPE string ASSERT is::email($value);\
                 DEFINE INDEX email ON TABLE users COLUMNS email UNIQUE;\
-                DEFINE FIELD cid ON users TYPE string;\
-                DEFINE INDEX cid ON TABLE users COLUMNS cid UNIQUE;\
+                DEFINE FIELD peer_id ON users TYPE string;\
+                DEFINE INDEX peer_id ON TABLE users COLUMNS peer_id UNIQUE;\
                 DEFINE FIELD name ON users TYPE string;\
                 DEFINE FIELD avatar ON users TYPE string;\
-                DEFINE FIELD password ON users TYPE string;\
+                DEFINE FIELD password_hash ON users TYPE string;\
                 DEFINE FIELD creation_date ON users TYPE string;\
                 DEFINE FIELD is_visible ON users TYPE bool;\
                 DEFINE FIELD is_inactive ON users TYPE bool;";
         db.query(sql).await?;
 
         Ok(())
+    }
+}
+
+#[allow(dead_code)]
+impl User {
+    /// Get a user from their Peer ID
+    ///
+    /// ## Arguments
+    ///
+    /// * `db` - The database connection
+    /// * `peer_id` - The user's peer id
+    ///
+    /// ## Returns
+    ///
+    /// * `Result<Option<User>, Error>` - The result of the operation
+    ///
+    /// ## Errors
+    ///
+    /// * `Error` - The error returned by the database
+    pub async fn get_from_peer_id(
+        db: ConnectionData,
+        peer_id: &str,
+    ) -> Result<Option<User>, Error> {
+        let mut res = db
+            .query("SELECT * FROM users WHERE peer_id=$id")
+            .bind(("id", peer_id))
+            .await?;
+        let user = res.take(0)?;
+
+        Ok(user)
+    }
+
+    /// Get a user from their email
+    ///
+    /// ## Arguments
+    ///
+    /// * `db` - The database connection
+    /// * `email` - The user's email
+    ///
+    /// ## Returns
+    ///
+    /// * `Result<Option<User>, Error>` - The result of the operation
+    ///
+    /// ## Errors
+    ///
+    /// * `Error` - The error returned by the database
+    pub async fn get_from_email(db: ConnectionData, email: &str) -> Result<Option<User>, Error> {
+        let mut res = db
+            .query("SELECT * FROM users WHERE email=$email")
+            .bind(("email", email))
+            .await?;
+        let user = res.take(0)?;
+
+        Ok(user)
+    }
+
+    /// Verify that the user's password is correct
+    ///
+    /// ## Arguments
+    ///
+    /// * `password` - The user's password
+    ///
+    /// ## Returns
+    ///
+    /// * `Result<(), argon2::password_hash::Error>` - The result of the operation
+    /// 
+    /// ## Errors
+    /// 
+    /// * `argon2::password_hash::Error` - The error returned by the password hashing library
+    pub fn verify_password(&self, password: String) -> Result<(), argon2::password_hash::Error> {
+        let argon2 = Argon2::default();
+
+        let hash = PasswordHash::new(&self.password_hash)?;
+
+        argon2.verify_password(password.as_bytes(), &hash)
     }
 }
--- src/utils/env.rs
+++ src/utils/env.rs
@@ -1,11 +1,30 @@
 use std::{collections::HashMap, env};
 
 /// Returns HashMap of Enviroment variables
+/// 
+/// ## Returns
+/// 
+/// * `HashMap<String, String>` - The enviroment variables
+/// 
+/// ## Panics
+/// Panics if the enviroment variables cannot be read
 fn get_envv() -> HashMap<String, String> {
     env::vars().map(|(key, value)| (key, value)).collect()
 }
 
 /// Returns either the value of an enviroment variable or the default provided
+/// 
+/// ## Arguments
+/// 
+/// * `key` - The key of the enviroment variable
+/// * `default` - The default value
+/// 
+/// ## Returns
+/// 
+/// * `String` - The value of the enviroment variable or the default
+/// 
+/// ## Panics
+/// Panics if the variable does not exist
 pub fn get_env_or(key: &str, default: &str) -> String {
     let envv = get_envv();
     if envv.contains_key(key) {
@@ -17,6 +36,14 @@ pub fn get_env_or(key: &str, default: &str) -> String {
 }
 
 /// Returns the value of an enviroment variable
+/// 
+/// ## Arguments
+/// 
+/// * `key` - The key of the enviroment variable
+/// 
+/// ## Returns
+/// 
+/// * `String` - The value of the enviroment variable
 ///
 /// ## Panics
 /// Panics if the variable does not exist
