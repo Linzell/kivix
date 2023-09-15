cc7a82b1a67a950043427e8b606261cbcdef9905
linzell <linzellart@gmail.com>
2023-09-15 17:29:23 +0200

    📝

--- src/error.rs
+++ src/error.rs
@@ -1,5 +1,18 @@
 use actix_web::{http::StatusCode, HttpResponse, ResponseError};
 
+/// Error type for the API
+/// 
+/// ## Fields
+/// 
+/// * `CtxFail` is the error type for when the context fails
+/// * `UntisError` is the error type for when fetching from Untis fails
+/// * `Surreal` is the error type for SurrealDB
+/// * `IO` is the error type for IO
+/// * `Reqwest` is the error type for Reqwest
+/// 
+/// ## Methods
+/// 
+/// * `error_response` returns the error response
 #[derive(thiserror::Error, Debug)]
 pub enum Error {
     #[error("Fail to get Ctx")]
@@ -18,6 +31,15 @@ pub enum Error {
     Reqwest(#[from] reqwest::Error),
 }
 
+/// The error response
+/// 
+/// ## Error response
+/// 
+/// * `404 Not Found` if the status code is 404
+/// * `403 Forbidden` if the status code is 403
+/// * `409 Conflict` if the status code is 409
+/// * `500 Internal Server Error` if the status code is 500
+/// * `500 Internal Server Error` if the status code is anything else
 impl ResponseError for Error {
     fn error_response(&self) -> HttpResponse<actix_web::body::BoxBody> {
         let code = self.status_code();
